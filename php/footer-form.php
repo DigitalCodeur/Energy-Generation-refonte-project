0701ecb5b54ee1406c87ae049809e36f13aaea5b
LOUMON-Koffi-Samuel <lkoffisamuel@gmail.com>
2022-07-10 20:03:29 +0000

    resolution des bugs

--- php/footer-form.php
+++ php/footer-form.php
@@ -1,73 +1,35 @@
 <?php
 
+if(!empty( $_POST["name-footer"]) && !empty($_POST["email-footer"])){
 
-if (!empty($_POST["name-footer"]) && !empty($_POST["email-footer"])) {
 
   $name = $_POST["name-footer"];
   $mail = $_POST["email-footer"];
   $french = $_POST["checkbox-prefere-french"];
   $english = $_POST["checkbox-prefere-english"];
 
-  // echo $name, $mail, $french, $english;
-
   $servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "inscription-newsletter";
+  $username = "root";
+  $password = "root";
+  $dbname = "db_remonte_eg";
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO newsletter (name, email, langage, langage2)
-  VALUES ('$name', '$mail', '$french','$english')";
-  // use exec() because no results are returned
-  $conn->exec($sql);
-  echo "New record created successfully";
-} catch(PDOException $e) {
-  echo $sql . "<br>" . $e->getMessage();
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "INSERT INTO  newsletter_form (name, email, langage1, langage2)
+    VALUES ('$name', '$mail', '$french','$english')";
+     // use exec() because no results are returned
+     $conn->exec($sql);
+     //echo "New record created successfully";
+  } catch(PDOException $e) {
+     echo $sql . "<br>" . $e->getMessage();
+    }
+
+   $conn = null;
+
+   echo "Merci de vous etes inscrit à notre Newsletter.";
 }
-
-$conn = null;
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ?>
