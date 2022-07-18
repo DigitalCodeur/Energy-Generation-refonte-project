fd962e7c0df937f527aa4ceffaadbc74e4e80543
LOUMON-Koffi-Samuel <lkoffisamuel@gmail.com>
2022-07-18 17:18:06 +0000

    modification après w3validator

--- php/energy-space.php
+++ php/energy-space.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/fonts.css" />
     <link rel="stylesheet" href="../css/style.css">
     <title>Energy Space</title>
 </head>
@@ -292,4 +291,4 @@
 
 </body>
 
-</php>
+</html>
